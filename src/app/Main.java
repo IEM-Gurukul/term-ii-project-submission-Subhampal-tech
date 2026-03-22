53fdee0d6efd78082cdd50124e84488b89ba0700
Subham Pal <spal08672@gmail.com>
2026-03-22 09:09:44 +0530

    Added core model classes: Player, Move, GameState

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,19 @@
 package app;
 
+import model.Player;
+import model.Move;
+import model.GameState;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Game Engine Starting...");
+
+        Player p1 = new Player(1, "Player1", 'X');
+        Player p2 = new Player(2, "Player2", 'O');
+
+        Move move = new Move(p1, 1, 2);
+
+        System.out.println(p1);
+        System.out.println("Move at: " + move.getRow() + "," + move.getCol());
+        System.out.println("Game State: " + GameState.ONGOING);
     }
 }
